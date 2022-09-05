a5e6c204a9a834cb38ce62fdc69363dfa4fea01b
Leandro T <leandrotalenton@hotmail.com>
2022-09-05 00:43:37 -0300

    se agrega la entrega de API RESTful

--- index.js
+++ index.js
@@ -1,50 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const productos = require("./routers/productos")
 
-let productos
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-/* _____________________(INICIO) se trae lo del desafio anterior_____________________ */
-const fs = require("fs");
+app.use("/api/productos", productos)
 
-class Container{
-    constructor(file){
-        this.file = file;
-    }
+app.use("/", express.static(__dirname + "/assets"))
 
-    // metodo getAll()
-    async getAll(){
-        const currentProducts = await fs.promises.readFile(this.file, "utf-8");
-        const products = JSON.parse(currentProducts);
-        productos = products
-    }
-}
-
-const fileProductos = "./productos.json";
-const contenedor = new Container(fileProductos);
-
-
-const ejecutar = async function(){
-    await contenedor.getAll()
-    console.log(`no hay problema willy`)
-}
-const test = ejecutar()
-/* _____________________(FIN) se trae lo del desafio anterior_____________________ */
-
-
-app.get("/", (req,res)=>{
-    res.send('ir a "/productos" para ver un listado de todos los productos, ir a "/productoRandom" para ver un producto al azar');
-})
-
-//ruta get /productos que devuelva un array con todos los productos disponibles en el servidor
-app.get("/productos", (req,res)=>{
-    res.send(productos);
-})
-
-//ruta get /productoRandom que devuelva un producto elegido al azar entre todos los productos disponibles
-app.get("/productoRandom", (req,res)=>{
-    res.send(productos[parseInt(Math.random()*productos.length)]);
-})
-
-const server = app.listen(8080, ()=>{
-    console.log("servidor express iniciado")
+app.listen(8080, ()=>{
+    console.log(`servidor iniciado`)
 })
---
+++ routers/productos.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const {Router} = express;
+const router = Router()
+
+let productos = []
+
+// devuelve todos los productos
+router.get("/", (req,res)=>{
+    res.send(productos)
+})
+
+//devuelve un producto segun su id
+router.get("/:id", (req,res)=>{
+    const { id } = req.params
+    const prodcuto = productos.find( p => p.id === parseInt(id))
+    if(prodcuto){
+        res.send(prodcuto)
+    } else {
+        res.status(404).send({error:`producto no encontrado`})
+    }
+})
+
+//recibe y agrega un producto, y lo devuelve con su id asignado
+router.post("/", (req,res)=>{
+    if(productos.length !== 0 ){
+        req.body.id = productos[productos.length-1]?.id+1
+    } else {
+        req.body.id = 1
+    }
+    productos.push(req.body)
+    res.send(req.body)
+})
+
+//recibe y actualiza un producto segun su id
+router.put("/:id", (req,res)=>{
+    const { id } = req.params
+    const prodcutToReplace = productos.find( p => p.id === parseInt(id))
+    if(prodcutToReplace){
+        req.body.id = prodcutToReplace.id
+        productos.splice(productos.indexOf(prodcutToReplace),1,req.body)
+        res.send(productos)
+    } else {
+        res.status(404).send({error:`producto no encontrado`})
+    }
+})
+
+//elimina un producto segun su id
+router.delete("/:id", (req,res)=>{
+    const { id } = req.params
+    const prodcutToDelete = productos.find( p => p.id === parseInt(id))
+    if(prodcutToDelete){
+        req.body.id = prodcutToDelete.id
+        productos.splice(productos.indexOf(prodcutToDelete),1)
+        res.send(productos)
+    } else {
+        res.status(404).send({error:`producto no encontrado`})
+    }
+})
+
+module.exports = router
