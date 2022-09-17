626b4392fb0b4a0d2e58374c7c656bb208138963
Leandro T <leandrotalenton@hotmail.com>
2022-09-17 16:19:00 -0300

    se cambia el engine a pug

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("views","./views")
-app.set("view engine","ejs")
+app.set("view engine","pug")
 
 app.use("/api/productos", productos)
 
