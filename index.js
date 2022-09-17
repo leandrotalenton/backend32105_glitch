77f64c2e33fd5c0b77e1f17516c3e5d1e8930a90
Leandro T <leandrotalenton@hotmail.com>
2022-09-17 18:42:41 -0300

    se agrega handlebar

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const app = express();
 const productos = require("./routers/productos")
+const handlebars = require("express-handlebars")
 
 const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine(
+    "hbs",
+    handlebars.engine({
+        extname:"hbs",
+        layoutsDir: __dirname + "/views", // capaz que hay que agregarle el ./
+        defaultLayout: null
+    })
+)
+
 app.set("views","./views")
-app.set("view engine","ejs")
+app.set("view engine","hbs")
 
 app.use("/api/productos", productos)
 
-app.get("/", (req, res)=>{
-    res.render(`./formulario`)
+app.use("/", (req, res)=>{
+    res.render(`formulario`)
 })
 
 app.listen(8080, ()=>{
